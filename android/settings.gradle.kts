6e466acbf1a4c179a4d8d9c4b2839e205809e266
您的GitHub用戶名 <GitHub>
2025-04-13 00:05:22 +0800

    Fix settings.gradle.kts to comply with Gradle requirements

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,13 @@
-pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
-    }
+// 定义 Flutter SDK 路径
+val flutterSdkPath = run {
+    val properties = java.util.Properties()
+    file("local.properties").inputStream().use { properties.load(it) }
+    val flutterSdkPath = properties.getProperty("flutter.sdk")
+    require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+    flutterSdkPath
+}
 
+pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
